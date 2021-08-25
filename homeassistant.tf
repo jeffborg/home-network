e195177eef19c6e6eecb70bd418bfbf0682a44ee
Jeffrey Borg <jeffrey@borgs.net>
2021-08-25 17:34:18 +1000

    home assistant install

---
+++ homeassistant.tf
@@ -0,0 +1,36 @@
+resource "helm_release" "homeassistant" {
+  name       = "home-assistant"
+  repository = "https://k8s-at-home.com/charts/"
+  chart      = "home-assistant"
+  version    = "10.0.0"
+  #   set {
+  #     name = "ingress.main.hosts.0.host"
+  #     value = "*"
+  #   }
+  values = [yamlencode({
+    env = {
+      TZ = "Australia/Sydney"
+    }
+    persistence = {
+      config = {
+        enabled   = true
+        mountPath = "/config"
+      }
+    }
+    ingress = {
+      main = {
+        enabled = true
+        hosts = [
+          {
+            host = "homeassistant.192.168.58.135.sslip.io"
+            paths = [
+              {
+                path = "/"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  })]
+}
