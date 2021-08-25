741f18400b929373457e7d98ae6ff5a24e5500d2
Jeffrey Borg <jeffrey@borgs.net>
2021-08-25 17:32:55 +1000

    persitant volumes on the nas

--- main.tf
+++ main.tf
@@ -21,9 +21,33 @@ provider "helm" {
   }
 }
 
-resource "kubernetes_namespace" "demo" {
-  metadata {
-    name = "my-first-namespace"
-  }
+variable "freenas_root_password" {
+  type        = string
+  description = "Freenas root password"
 }
 
+variable "freenas_host" {
+  type        = string
+  description = "IP address for freenas"
+}
+
+resource "helm_release" "democratic-csi" {
+  repository       = "https://democratic-csi.github.io/charts/"
+  namespace        = "democratic-csi"
+  create_namespace = true
+  name             = "democratic-csi"
+  chart            = "democratic-csi"
+  version          = "0.8.3"
+  timeout = 600 # wait 10 minutes
+  values = [templatefile("freenas-nfs.yaml", {
+    host     = var.freenas_host
+  })]
+  set_sensitive {
+    name = "driver.config.httpConnection.password"
+    value = var.freenas_root_password
+  }
+  set_sensitive {
+    name = "driver.config.sshConnection.password"
+    value = var.freenas_root_password
+  }
+}
