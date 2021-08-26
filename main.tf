761f81edddb229d97bd92867cdb4b2e3db863767
Jeffrey Borg <jeffrey@borgs.net>
2021-08-26 17:29:59 +1000

    added flux and enable experimental mode for k8s

--- main.tf
+++ main.tf
@@ -8,17 +8,27 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.4.1"
     }
+    flux = {
+      source = "fluxcd/flux"
+      version = "0.2.2"
+    }    
   }
 }
 
 provider "kubernetes" {
   config_path = "./k8s/anisible/static/kubectl.conf"
+  experiments {
+    manifest_resource = true
+  }
 }
 
 provider "helm" {
   kubernetes {
     config_path = "./k8s/anisible/static/kubectl.conf"
   }
+}
+
+provider "flux" {
 }
 
 variable "freenas_root_password" {
@@ -38,16 +48,16 @@ resource "helm_release" "democratic-csi" {
   name             = "democratic-csi"
   chart            = "democratic-csi"
   version          = "0.8.3"
-  timeout = 600 # wait 10 minutes
+  timeout          = 600 # wait 10 minutes
   values = [templatefile("freenas-nfs.yaml", {
-    host     = var.freenas_host
+    host = var.freenas_host
   })]
   set_sensitive {
-    name = "driver.config.httpConnection.password"
+    name  = "driver.config.httpConnection.password"
     value = var.freenas_root_password
   }
   set_sensitive {
-    name = "driver.config.sshConnection.password"
+    name  = "driver.config.sshConnection.password"
     value = var.freenas_root_password
   }
 }
