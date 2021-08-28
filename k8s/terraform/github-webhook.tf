0fae7d7eb721861741b6cb449763a88f769c1fd0
Jeffrey Borg <jeffrey@borgs.net>
2021-08-28 13:34:33 +1000

    webhook install

--- k8s/terraform/github-webhook.tf
+++ k8s/terraform/github-webhook.tf
@@ -1,23 +1,19 @@
 
 
-# TOKEN=$(head -c 12 /dev/urandom | shasum | cut -d ' ' -f1)
-# echo $TOKEN
-
-# kubectl -n flux-system create secret generic  \	
-# --from-literal=token=$TOKEN
-
 resource "random_password" "webhook-token" {
   length = 40
   special = true
 }
 
-# resource "github_repository_webhook" "main" {
-#     repository = data.github_repository.main.name
-
-#     configuration {
-#       url = "${var.webhook_external_base_url}/"
-#     }
-# }
+resource "github_repository_webhook" "main" {
+    count = var.webhok_target_url != null ? 1 : 0
+    repository = data.github_repository.main.name
+    events = [ "push" ]    
+    configuration {
+      url = "${var.webhook_external_base_url}${var.webhok_target_url}"
+      secret = random_password.webhook-token.result      
+    }
+}
 
 # push secret into k8s
 resource "kubernetes_secret" "webhook-token" {
@@ -30,7 +26,3 @@ resource "kubernetes_secret" "webhook-token" {
   }
 }
 
-# yamldecode(kubectl_manifest.sync["kustomize.toolkit.fluxcd.io/v1beta1/kustomization/flux-system/flux-system"].yaml_body_parsed).spec.sourceRef.name
-# resource "kubernetes" "name" {
-  
-# }
