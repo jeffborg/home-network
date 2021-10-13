8676cb6e02e916d50e2aadc981d0334deb30dc4d
Jeffrey Borg <jeffrey@borgs.net>
2021-10-13 11:00:39 +1100

    pre ansible terraform stage for etcd snapshot backups

---
+++ k8s/stage1-terraform/cluser-snapshots.tf
@@ -0,0 +1,53 @@
+
+# require
+# iam user
+# s3 bucket
+
+# export var file for ansible
+
+# s3 bucket for storage
+resource "aws_s3_bucket" "cluster_snapshot_backups" {
+  bucket_prefix = "cluster-snapshots-backups"
+  acl           = "private"
+  force_destroy = false
+}
+
+# IAM USER to access this bucket
+resource "aws_iam_user" "cluster_snapshots_buckets_user" {
+  name          = "cluster-snapshots-backups-user"
+  force_destroy = true
+}
+
+# keys for this user
+resource "aws_iam_access_key" "cluster_snapshots_buckets_user" {
+  user = aws_iam_user.cluster_snapshots_buckets_user.name
+}
+
+# aws user to access the bucket
+data "aws_iam_policy_document" "cluster_snapshots_buckets_policy" {
+  statement {
+    actions = [
+      "s3:PutObject*",
+      "s3:GetBucket*",
+      "s3:List*",
+      "s3:Abort*",
+      "s3:GetObject*"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cluster_snapshot_backups.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.cluster_snapshot_backups.bucket}/*"
+    ]
+  }
+}
+
+# policy to access the s3 bucket
+resource "aws_iam_policy" "cluster_snapshots_buckets_policy" {
+  name_prefix = "cluster-snapshosts-bucket-backup-access-"
+  policy      = data.aws_iam_policy_document.cluster_snapshots_buckets_policy.json
+}
+
+# attach policy to user
+resource "aws_iam_user_policy_attachment" "cluster_snapshots_buckets_policy_attachment" {
+  user       = aws_iam_user.cluster_snapshots_buckets_user.name
+  policy_arn = aws_iam_policy.cluster_snapshots_buckets_policy.arn
+}
---
+++ k8s/stage1-terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "jeffborg"
+
+    workspaces {
+      name = "home-network"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = local.aws_region
+  profile = "personal"
+}
+
+locals {
+  aws_region = "ap-southeast-2"
+}
