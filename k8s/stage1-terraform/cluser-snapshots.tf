f16d6cac2176cfdacd2f20e539fc558d84159f13
Jeffrey Borg <jeffrey@borgs.net>
2021-11-06 13:28:21 +1100

    k3s now supports s3 timeout

--- k8s/stage1-terraform/cluser-snapshots.tf
+++ k8s/stage1-terraform/cluser-snapshots.tf
@@ -63,6 +63,7 @@ resource "local_file" "aws_bucket_paramaters" {
         etcd-s3-bucket = aws_s3_bucket.cluster_snapshot_backups.bucket
         etcd-s3-region = local.aws_region
         etcd-s3-folder = "/snapshots"
+        etcd-s3-timeout = "5m"
         kube-apiserver-arg = "feature-gates=MixedProtocolLBService=true"
         disable = [
             "local-storage",
